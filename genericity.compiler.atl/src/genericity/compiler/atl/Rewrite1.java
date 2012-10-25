5dad6ae0873895eb844c4c28aae6a6c3a04c3b02
jesus <jesus.sanchez.cuadrado@uam.es>
2012-10-25 16:36:06 +0200

    Added extractors to XMI from GBind to ATL (as a pop-up menu in Eclipse)

---
+++ genericity.compiler.atl/src/genericity/compiler/atl/Rewrite1.java
@@ -0,0 +1,119 @@
+package genericity.compiler.atl;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclectic.idc.datatypes.JavaListConverter;
+import org.eclectic.idc.jvm.runtime.IMethodWrapper;
+import org.eclectic.idc.jvm.runtime.IModelMethodHandler;
+import org.eclectic.idc.jvm.runtime.IdcException;
+import org.eclectic.idc.jvm.runtime.IdcMetaclass;
+import org.eclectic.modeling.emf.BasicEMFModel;
+import org.eclectic.modeling.emf.EMFLoader;
+import org.eclectic.modeling.emf.IModel;
+import org.eclectic.modeling.emf.ModelManager;
+import org.eclectic.modeling.emf.NoModelFoundException;
+import org.eclectic.modeling.emf.Util;
+
+import eclectic.callexpr;
+import eclectic.composed_genericity;
+import eclectic.ocl2ocl;
+import eclectic.rewrite_class1;
+
+public class Rewrite1 {
+	
+	public static void main(String[] args) throws IOException {
+	     System.setProperty("org.apache.commons.logging.Log",
+                 "org.apache.commons.logging.impl.NoOpLog");
+
+	    rewrite_class1 transformation = new rewrite_class1();
+	     
+		Util.registerResourceFactory();
+		ModelManager manager = new ModelManager();
+		EMFLoader    loader  = new EMFLoader(new JavaListConverter());
+		BasicEMFModel in  = loader.basicModelFromFile(
+				new String[] { 
+						// withDir("../be.ac.vub.simpleocl/metamodel/simpleocl.ecore"),		
+						withDir("../genericity.language.gbind/metamodel/gbind.ecore")
+				}, 
+				withDir("test/uml2java/generalization.concrete/uml2java.gbind.xmi")); 
+		
+		BasicEMFModel inout = loader.basicModelFromFile(
+				withDir("../genericity.atl.transformations/metamodels/ATL.ecore"), 
+				withDir("test/uml2java/uml2java.atl.xmi")); 
+		
+		// in.registerMethodHandler(new BasicMethodHandler(manager));
+		manager.register("gbind", in);
+		manager.register("atl", inout);
+		in.registerMethodHandler(new BasicMethodHandler(manager));
+		inout.registerMethodHandler(new BasicMethodHandler(manager));
+		transformation.setModelManager(manager);
+		
+		transformation.configure_();
+		transformation.start_();
+
+		// TODO: Define in/out modes for EMFModels
+		// out.serialize();
+		
+	}
+
+	private static String withDir(String path) {
+		return "." + File.separator + path;
+	}
+	
+
+public static class BasicMethodHandler implements IModelMethodHandler {
+
+	protected ModelManager manager;
+
+	// This can be more efficient, because sometimes the model is known at compile time
+	public BasicMethodHandler(ModelManager m) {
+		this.manager = m;
+	}
+	
+	@Override
+	public IMethodWrapper wrap(Object o) {
+		try {
+			return new BasicMethodWrapper(manager.getNamespace(o), o);
+		} catch (NoModelFoundException e) {
+			throw new IdcException(e);
+		}
+	}
+	
+
+}
+
+public static class BasicMethodWrapper implements IMethodWrapper {
+	protected IModel<Object, ?> model;
+	protected Object object;
+
+	public BasicMethodWrapper(IModel<?, ?> model, Object o) {
+		this.model = (IModel<Object, ?>) model;
+		this.object = o;
+	}
+	
+	public Boolean kind_of(Object o) {
+		if ( ! (o instanceof IdcMetaclass) ) return Boolean.FALSE;
+		return model.isKindOf(object, ((IdcMetaclass) o).getMetaclassName());
+	}
+	
+	public Object _refContainer() {
+		return model.getContainer(object);
+	}
+	
+	public String println(String x) {
+		String s = x + ": " + object;
+		System.out.println(s);
+		return s;
+		
+	}
+	
+	public Boolean is_nil() { return false; }
+
+	public Object if_nil() { return object; }
+
+	public Boolean eq(Object o) { return o == object; }
+}
+
+
+}
