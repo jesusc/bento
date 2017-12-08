3764d4a357a6555bd9cfb7c21a3e2cfad2c02af2
Jesus <jesus.sanchez.cuadrado@gmail.com>
2017-12-08 10:46:23 +0100

    save

--- plugins/genericity.language.gbind.resource.ui/src/gbind/resource/gbind/ui/custom/ValidateBinding.java
+++ plugins/genericity.language.gbind.resource.ui/src/gbind/resource/gbind/ui/custom/ValidateBinding.java
@@ -2,29 +2,22 @@ package gbind.resource.gbind.ui.custom;
 
 import java.util.List;
 
-import gbind.dsl.BindingModel;
-import gbind.resource.gbind.ui.GbindEditor;
-import genericity.language.gbind.GbindEProblemSeverity;
-import genericity.language.gbind.GbindEProblemType;
-import genericity.language.gbind.IGbindTextResource;
-import genericity.language.gbind.mopp.GbindProblem;
-
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 
-import bento.language.bentocomp.resource.bento.ResolveBentoQuickfix;
 import bento.utils.BindingUtils;
 import bento.validators.BindingValidationProblem;
 import bento.validators.BindingValidator;
+import gbind.dsl.BindingModel;
+import genericity.language.gbind.GbindEProblemSeverity;
+import genericity.language.gbind.GbindEProblemType;
+import genericity.language.gbind.IGbindTextResource;
+import genericity.language.gbind.mopp.GbindProblem;
 
 public class ValidateBinding implements IObjectActionDelegate {
 	private Shell shell;
